33cd9bba93ab68fdf40bf37cb81cd2bd9598de76
Sangwan Jeon <commits@swjeon.kr>
2023-12-07 14:54:37 +0900

    wip: what were I doing?

---
+++ src/pincfg.h
@@ -0,0 +1,21 @@
+#ifndef _SKKU_BUS_PINCFG_H_
+#define _SKKU_BUS_PINCFG_H_
+
+#define R0 15
+#define G0 16
+#define B0 17
+#define R1 18
+#define G1 19
+#define B1 20
+
+#define A 26
+#define B 27
+#define C 28
+#define D 29
+// #define E 12
+
+#define OE 5
+#define CLK 6
+#define LAT 7
+
+#endif // _SKKU_BUS_PINCFG_H_
